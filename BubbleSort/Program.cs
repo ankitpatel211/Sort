808eb562b52c540dc363f9a45bc249183ea9c5ec
Ankit Patel <wd.ankit.patel@gmail.com>
2025-08-20 10:50:14 +0530

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static int[] BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            for (int j = 0; j < n; j++) {
+                for (int i = 0; i < n - 1; i++) {
+                    int left = i;
+                    int right = i + 1;
+                    if (array[left] > array[right]) {
+                        int temp = array[left];
+                        array[left] = array[right];
+                        array[right] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 2, 7, 1 };
+            Console.Write("Unsorted Elements: ");
+            foreach (int element in array) {
+                Console.Write(element + " ");
+            }
+            int[] sortArray = BubbleSort(array);
+            Console.Write("\nSorted Elements: ");
+            foreach (int element in sortArray) {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    // See https://aka.ms/new-console-template for more information
+
+    class Program
+    {
+        static int[] InsertionSort(int[] unsortedElements)
+        {
+            for (int i = 0; i < unsortedElements.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && unsortedElements[j] < unsortedElements[j - 1])
+                {
+                    int temp = unsortedElements[j];
+                    unsortedElements[j] = unsortedElements[j - 1];
+                    unsortedElements[j - 1] = temp;
+                    j--;
+                }
+            }
+            return unsortedElements;
+        }
+        static void Main(string[] args)
+        {
+            int[] unsortedElements = { 5, 12, 3, 8, 1, 19, 7, 14, 
+                2, 10, 16, 4, 11, 6, 18, 9, 13, 17, 0, 15 };
+            int[] sortedArray = InsertionSort(unsortedElements);
+            Console.Write("Sorted Elements: ");
+            foreach (int element in sortedArray) {
+                Console.Write(element + " ");
+            }
+        }
+    }
+
+}
